deb092f583337af9b2281c73796e60dd31953621
idiot <qpalqpzm999@gmail.com>
2023-11-30 21:43:40 +0700

    ну вроде ок

---
+++ src/student.java
@@ -0,0 +1,15 @@
+
+public class student {
+
+        private String fio;
+        attestat att = new attestat();
+        student() {}
+        public void setFio(String nFio) {
+           fio = nFio;
+        }
+        public String getFio() {
+            return fio;
+        }
+
+
+}
